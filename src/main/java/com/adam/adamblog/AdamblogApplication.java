5c7e614c21940c8a62832374e776036f4e290f37
adam <starksmile@hotmail.com>
2018-02-23 20:05:50 +0800

    First commit

---
+++ src/main/java/com/adam/adamblog/AdamblogApplication.java
@@ -0,0 +1,12 @@
+package com.adam.adamblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdamblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdamblogApplication.class, args);
+	}
+}
---
+++ src/test/java/com/adam/adamblog/AdamblogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adam.adamblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdamblogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

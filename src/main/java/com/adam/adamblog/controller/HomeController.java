6b2345b4296284e87c4cad636cbfaff11da6a4c0
adam <starksmile@hotmail.com>
2018-02-27 23:47:04 +0800

    Make a default layout with navbar and footer.

---
+++ src/main/java/com/adam/adamblog/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.adam.adamblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author adam
+ * 创建于 2018-02-23 20:17.
+ * 处理根路径的控制器。
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String gotoIndexPage() {
+        return "index";
+    }
+
+}
